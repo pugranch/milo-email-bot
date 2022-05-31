f76a4e0e65e91e83477bbeb565904034330bf499
Chris Millar <cmillar@adobe.com>
2022-05-30 18:19:11 -0600

    test

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:18.2.0-buster-slim
-
-COPY . .
-
-RUN npm install
-
-ENTRYPOINT ["node", "/action.js"]
--- action.js
+++ action.js
@@ -1,4 +1,7 @@
+import github from '@actions/github';
+
 async function run() {
-  console.log('ive been updated and am running');
+    const { ref } = github.context.payload.pull_request.head;
+    console.log(github.context.payload.pull_request);
 }
 run();
