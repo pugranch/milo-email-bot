15872c9209ba219a76418356204452eabf4b463d
Chris Millar <cmillar@adobe.com>
2022-05-30 18:22:47 -0600

    wip

--- action.js
+++ action.js
@@ -1,7 +1,8 @@
 import github from '@actions/github';
 
 async function run() {
-    const { ref } = github.context.payload.pull_request.head;
-    console.log(github.context.payload.pull_request);
+    const { title, body } = github.context.payload.pull_request;
+    console.log(title);
+    console.log(body);
 }
 run();
